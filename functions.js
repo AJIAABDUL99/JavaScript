ae44d10fe87978995556831fdc90f2fad25354b3
AJIAABDUL99 <ajiamoha254@gmail.com>
2024-07-08 11:10:58 +0300

    add funcyions.js

---
+++ functions.js
@@ -0,0 +1,14 @@
+// function displayName() {
+//     name ="John Doe";
+//     return name;
+// }
+// console.log(displayName());
+
+function displaySum() {
+    var a = 32;
+    var b = 24;
+    
+    return (sum = a + b);
+}
+console.log(displaySum());
+
---
+++ variables.js
@@ -0,0 +1,11 @@
+namesInClass =["Antony","Constance",21,true,null,"Jasmine","Katie"];
+
+erustusAge=21;
+IsKenyan =true;
+
+console.log(typeof erustusAge);
+
+console.log(typeof nameInClass);
+
+console.log(typeof isKenyan);
+
